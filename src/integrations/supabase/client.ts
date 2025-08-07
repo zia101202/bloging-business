52b38765a4daf823669e545399760fe173d051ca
ziaurreh <ziaurrehman00112244@gmail.com>
2025-08-07 13:07:19 +0500

    m

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL =process.env.VITE_SUPABASE_URL 
-const SUPABASE_PUBLISHABLE_KEY = process.env.VITE_SUPABASE_PUBLISHABLE_KEY
+const SUPABASE_URL =import.meta.env.VITE_SUPABASE_URL 
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
